7851b0cc28c5b6fc3bc3ee4c6b46395e912f631a
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-24 22:05:30 +0530

    Threads - Pausing a Thread

---
+++ src/com/srihari/threads/multithreading/pause/PauseApplication.java
@@ -0,0 +1,13 @@
+package com.srihari.threads.multithreading.pause;
+
+public class PauseApplication {
+    public static void main(String[] args) {
+        System.out.println("Before initiating. Thread Name: "+Thread.currentThread().getName());
+
+        // 1o threads will be completed in 5 sec, instead of 50 sec (in case of sequential)
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new DownloadFileTask());
+            thread.start();     // new Thread will be initiated
+        }
+    }
+}
