52109dd57ed21d14e6c52bbb8992106f7a5c99dc
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-25 13:11:39 +0530

    Threads - Race condition in collections - ConcurrentCollection

---
+++ src/com/srihari/threads/multithreading/collection/concurrent/ConcurrentCollectionApplication.java
@@ -0,0 +1,32 @@
+package com.srihari.threads.multithreading.collection.concurrent;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentCollectionApplication {
+    public static void main(String[] args) {
+        // It uses a partitioning technique, doesn't use synchronization
+        // and different threads can access different methods a the same time (unlike SynchronzedCollection)
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+
+        Thread thread1 = new Thread(() -> {
+            map.putAll(Map.of(1, "a"));
+        });
+        Thread thread2 = new Thread(() -> {
+            map.putAll(Map.of(2, "b"));
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // All the values may not be printed, due to race condition
+        System.out.println(map);
+    }
+}
